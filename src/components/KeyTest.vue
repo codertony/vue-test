f051079bce650d667f5d2b3a069df5d3bffeca09
杨天宇 <tianyu.yang1@geely.com>
2019-07-14 00:17:57 +0800

    vue内部崩溃测试

---
+++ src/components/KeyTest.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <button @click="list=list1">切换1</button>
+    <button @click="list=list2">切换2</button>
+    <TestList :list="list"/>
+    {{test}}
+</div>
+</template>
+<script>
+    import TestList from './TestList'
+  export default {
+    name: 'key-test',
+    data(){
+      return {
+        list: [],
+        list1: [1,2,1],
+        list2: [2,2],
+      }
+    },
+    props: ['test'],
+    components: {
+      TestList
+    },
+    mounted() {
+      this.list = this.list1
+    }
+  }
+</script>
---
+++ src/components/TestList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-for="n in list" :key="n">
+            {{n}}
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'testList',
+    props: ['list']
+  }
+</script>
+
+<style scoped>
+
+</style>
